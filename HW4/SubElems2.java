7aa0e065cb73dd0e9c2d25e8f074a1c6e8dd3aa3
Austin Fernandez <ryanaustinf@yahoo.com>
2016-02-18 00:06:48 +0800

    SubElems2 change

--- HW4/SubElems2.java
+++ HW4/SubElems2.java
@@ -2,7 +2,8 @@ import java.util.List;
 import java.util.Iterator;
 
 public class SubElems2 extends NonTerminal {
-	
+	private Iterator<Elem> itr;
+
 	public SubElems2(String pattern) {
 		super("SUBELEMS2",pattern);
 	}
@@ -10,7 +11,19 @@ public class SubElems2 extends NonTerminal {
 	public void interpret() throws Exception {
 		if(!isSet()) {
 			throw new Exception(NOT_SET_MESSAGE);
-		} 
+		} else {
+			try {
+				Elems e = (Elems)getComponent("ELEMS");
+				e.interpret();
+				itr = e.getElems();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public Iterator<Elem> getElems() {
+		return itr;
 	}
 
 	public void build(List<Elem> elems) {
