702d71a5f0c234f94e276f9a35886912a75db36d
GoldenRayquaza12 <johansson_tan@dlsu.edu.ph>
2016-02-18 11:47:42 +0800

    Prog

---
+++ HW4/Prog.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Prog extends NonTerminal {
+
+	public Prog(String pattern) {
+		super("PROG",pattern);
+	}
+
+	public void interpret() throws Exception {
+		if(!isSet()) {
+			throw new Exception(NOT_SET_MESSAGE);
+		} else {
+			switch(getProdString()) {
+				case "ELEMS nl PLAY SUBELEMS2 NL":
+					Elems e = (Elems)getComponent("ELEM");
+                    e.interpret();
+					SubElems2 se2_1 = (SubElems2)getComponent("SUBELEMS2");
+					se2_1.interpret();
+                    Play play1 = (Play)getComponent("PLAY");
+                    play1.interpret();
+                    break;
+                case "PLAY SUBELEMS2 NL":
+					SubElems2 se2_2 = (SubElems2)getComponent("SUBELEMS2");
+                    se2_2.interpret();
+                    Play play2 = (Play)getComponent("PLAY");
+                    play2.interpret();
+                    break;
+                case "nl PROG NL":
+                    Prog pr = (Prog)getComponent("PROG");
+                    pr.interpret();
+                    break;
+				default:
+			}
+		}
+	}
+}
+
